d5e15729af5c266cf9945c18c80df6e650165448
John-Kim <john@leftium.com>
2022-02-16 14:55:20 +0900

    Add coffeescript.
    npx svelte-add@latest coffeescript

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,28 @@
-import adapter from '@sveltejs/adapter-auto';
+import coffee from "vite-plugin-coffee";
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+
+    vite: {
+      plugins: [
+        coffee({
+          jsx: false,
+        }),
+      ],
+    },
+  },
+
+  preprocess: [
+    preprocess({
+      coffeescript: {
+        bare: true,
+      },
+    }),
+  ],
 };
 
 export default config;
