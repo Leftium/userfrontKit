ce1fafcf56bcce1e8f98fcca09aabc70745a1ced
John-Kim <john@leftium.com>
2022-02-17 02:59:01 +0900

    Start protecting route.

--- src/routes/dashboard/index.svelte
+++ src/routes/dashboard/index.svelte
@@ -1,1 +1,14 @@
+<script context="module">
+    export function load({ session }) {
+        if (!session.user) {
+            return {
+                status: 302,
+                redirect: '/login'
+            };
+        }
+
+        return {};
+    }
+</script>
+
 <h1>Dashboard</h1>
