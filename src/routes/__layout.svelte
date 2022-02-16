44318ab7cc022ef4dd5fee58862aa21e54b2121a
John-Kim <john@leftium.com>
2022-02-16 15:42:52 +0900

    Add basic routes.

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<header>
+    <nav>
+        <li class:active={$page.url.pathname === '/'}><a href='/'>Home</a></li>
+        <li class:active={$page.url.pathname === '/login'}><a href='/login'>Login</a></li>
+        <li class:active={$page.url.pathname === '/reset'}><a href='/reset'>Reset</a></li>
+        <li class:active={$page.url.pathname === '/dashboard'}><a href='/dashboard'>Dashboard</a></li>
+    </nav>
+</header>
+
+<main>
+    <slot />
+</main>
+
+<style>
+    .active {
+        font-weight: bold;
+    }
+</style>
+
