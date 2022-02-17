14acec0da9634e9f3e244220e6703194eb646fc1
John-Kim <john@leftium.com>
2022-02-17 18:00:11 +0900

    Style with CSS.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,6 @@
 <script context='module'>
+    import '../global.css'
+
     import { page } from '$app/stores';
     import { config } from '$lib/UserfrontSvelte'
 
@@ -26,7 +28,54 @@
 </main>
 
 <style>
-    .active {
-        font-weight: bold;
-    }
+nav {
+  list-style: none;
+  background-color: #DC322F;
+  text-align: center;
+  padding: 0;
+  margin: 0;
+}
+li {
+  font-family: 'Oswald', sans-serif;
+  font-size: 1.2em;
+  line-height: 40px;
+  height: 40px;
+  border-bottom: 1px solid #888;
+}
+
+a {
+  text-decoration: none;
+  color: #fff;
+  display: block;
+  transition: .3s background-color;
+}
+
+a:hover {
+  background-color: #fff;
+  color: #DC322F;
+}
+
+li.active a {
+  background-color: #fff;
+  color: #DC322F;
+  cursor: default;
+}
+
+@media screen and (min-width: 600px) {
+  li {
+    width: 120px;
+    border-bottom: none;
+    height: 50px;
+    line-height: 50px;
+    font-size: 1.4em;
+  }
+  /* Option 1 - Display Inline */
+  li {
+    display: inline-block;
+    margin-right: -4px;
+  }
+}
+main {
+    margin: 16px;
+}
 </style>
