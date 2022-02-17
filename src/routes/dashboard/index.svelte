bf01a6fec6b371dcd3ee3f8b0b286a1c0468baf1
John-Kim <john@leftium.com>
2022-02-17 16:51:26 +0900

    Add user details on dashboard.

--- src/routes/dashboard/index.svelte
+++ src/routes/dashboard/index.svelte
@@ -3,8 +3,11 @@
 
     import { browser } from '$app/env'
 
+    let user = null;
+
     export function load({ session }) {
         let loggedIn = !!Userfront.tokens.accessToken;
+        user = Userfront.user;
 
         if (browser && !loggedIn) {
             return {
@@ -19,4 +22,11 @@
 
 <h1>Dashboard</h1>
 
+<div>
+    <h3>{ user.name }</h3>
+    <img src='{user.image}' />
+</div>
+
 <LogoutButton />
+
+<pre><code>{JSON.stringify(user, null, 4)}</code></pre>
